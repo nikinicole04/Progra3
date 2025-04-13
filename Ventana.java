bc36eb4241fc983c0962d9b54a6c63cc20d60f30
nikinicole04 <129918152+nikinicole04@users.noreply.github.com>
2025-04-13 15:40:54 -0500

    Add files via upload

--- Ventana.java
+++ Ventana.java
@@ -4,57 +4,66 @@ import java.awt.event.ActionListener;
 
 public class Ventana {
     private JPanel principal;
-    private JTextArea txtCodigo;
-    private JButton btnComprobar;
-    private JLabel CODIGO;
-
+    private JComboBox cboMarca;
+    private JTextField txtAnio;
+    private JButton btnEncolar;
+    private JButton btnDesencolar;
+    private JTextArea txtListado;
+    private JLabel lblPago;
+    private ColaAutos autos=new ColaAutos();
     public Ventana() {
-
-        btnComprobar.addActionListener(new ActionListener() {
+        btnEncolar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Pila pilas = new Pila();
+                try {
+                    String marca=cboMarca.getSelectedItem().toString();
+                    String anioTexto = txtAnio.getText().trim();
 
-
-                String texto = txtCodigo.getText();
-                for(int i = 0; i < texto.length(); i++) {
-                    char letra = texto.charAt(i);
-                    if (letra == '(' || letra == ')'
-                            || letra == '{' || letra == '}'
-                            || letra == '[') {
-                        //
-                    } else {
-                        if (letra == ',') {
-                            char salida = pilas.extraer().charAt(0);
-                            if (salida != '(') {
-                                JOptionPane.showMessageDialog(null, "Codigo incorrecto");
-                                return;
-
-                            }
-                        } else {
-                            if (letra == ',') {
-                                char salida = pilas.extraer().charAt(0);
-                                if (salida != '(') {
-                                    JOptionPane.showMessageDialog(null, "Codigo incorrecto");
-                                    return;
-                                }
-
-                            }
-                        }
+                    if (anioTexto.isEmpty()){
+                        throw new IllegalArgumentException("Debes ingresar un año");
                     }
-
+                    int anio;
+                    try {
+                        anio = Integer.parseInt(anioTexto);
+                    } catch (NumberFormatException ex) {
+                        // Se usa IllegalArgumentException porque es una excepción mas precisa porque indica que un argumento
+                        // proporcionado a un metodo no es válido
+                        throw new IllegalArgumentException("El año debe ser un número válido");
+                    }
+                    if (anio > 2025) {
+                        throw new IllegalArgumentException("El año no puede ser mayor a 2025");
+                    }
+                    autos.encolar(new Auto(marca,anio));
+                    txtListado.setText(autos.listatodos());
+                } catch (IllegalArgumentException ex) {
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
                 }
 
+            }
+        });
+        btnDesencolar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    Auto atendido=autos.desencolar();
+                    int anio = atendido.getAnio();
+                    int pago = 50 + (2025 - anio) * 50;
+                    lblPago.setText("Último auto atendido: \n"  + atendido.toString() + "\t Pago: \n$" + pago);
+                    txtListado.setText(autos.listatodos());
+                } catch (Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
 
+            }
         });
     }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ventana");
         frame.setContentPane(new Ventana().principal);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        frame.setSize(600, 800);
+        //frame.pack();
         frame.setVisible(true);
     }
-
-
 }
